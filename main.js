268677b41215e82f2684af6a9ea7b4d0866cfb71
Alexander <olexander.pomazkin@yandex.ru>
2022-02-16 12:47:13 +0700

    5

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-let arr = {}
-const n = 10
+ let arr =[]
 
+ function generate(){
+  let n=parseInt(document.getElementById('age').value)
 
-for (let i = 0; i<n; i++){
-	arr[i]=Math.floor(Math.random()*10)
+  for(let i=0;i<n;i++){
+  	arr[i]=Math.floor(Math.random()*10)
+  }
+
+  let div=document.getElementById('enner')
+  for(let i=0;i<n;i++){
+  	div.innerHTML+=arr[i]+' '
+  }
 }
-
-console.log(arr)
