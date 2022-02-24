0f0270bf3f83d95370f3e5944d4e8f8fe24b8535
Alexander Pomazkin <kjkdsk@yandex.ru>
2022-02-24 17:13:30 +0700

    7

--- main.js
+++ main.js
@@ -1,27 +1,38 @@
- let arr =[]
+function show() {
+    let age = parseInt(document.getElementById('age').value);
+    let div = document.getElementById('em');
+    let span = document.getElementById('me');
+    //let divv = document.getElementsByClassName('me');
+    let i;
+    let s = 0;
+    let arr = [];
 
- function generate(){
-  let n=parseInt(document.getElementById('age').value)
+    for (let i = 0; i < age; i++) {
+        arr[i] = Math.floor(Math.random() * 10);
+    }
+    div.innerHTML=" "
+    for (i = 0; i < age; i++) {
+        div.innerHTML += arr[i]+'\n'
+    }
 
-  for(let i=0;i<n;i++){
-  	arr[i]=Math.floor(Math.random()*10)
-  }
-  print()
+    for (i = 0; i < age; i++) {
+        s += arr[i] + 1;
+    }
+    console.log(s)
+    span.innerHTML = "\n"+`<p>${s}</p>`;
+
 }
-function zerofill(){
-	let n=parseInt(document.getElementById('age').value)
-  let div=parseInt(document.getElementById('enner').value)
-  for(let i=0;i<n;i++){
-  	arr[i]=0
-  }
-  print()
-}
-function print(){
-	let n=parseInt(document.getElementById('age').value)
-	let div=document.getElementById('enner')
-	div.innerHTML=''
-		for (let i=0;i<n;i++){
-			div.innerHTML+=arr[i]+' '
-		}
+function nulls() {
+    let age = parseInt(document.getElementById('age').value);
+    let div = document.getElementById('em');
+    let arr = [];
 
+    for (let i = 0; i < age; i++) {
+        arr[i] = 0;
+    }
+
+    div.innerHTML = " "
+    for (i = 0; i < age; i++) {
+        div.innerHTML += arr[i] + '\n'
+    }
 }
