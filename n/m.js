dd0b2775ece9bb25ad9f01a4199c9e0ea00b8438
Alexander <olexander.pomazkin@yandex.ru>
2022-02-25 13:26:02 +0700

    10

--- n/m.js
+++ n/m.js
@@ -1,6 +1,35 @@
-let arr=[
-	{'id':1,'name':'Данила','surname':'Алферов'}
+let students=[
+	{'id':1,'name':'Данила','surname':'Алферов'},
+	{'id':2,'name':'Данил','surname':'Алферо'},
+	{'id':3,'name':'Дани','surname':'Алфера'}
 ]
-for (let i=0; i<arr.length;i++){
-	console.log(arr[i].name)
+
+let current=0
+function load_student(id){
+	document.getElementById('name').value=students[id].name
+	document.getElementById('surname').value=students[id].surname
+}
+
+function next(){
+	if(current < students.length){
+		load_student(current)
+		current++
+	}
+	else{
+		document.getElementById('f').disamble=true
+
+	}
+}
+
+function ret(){
+	current=0
+	return next()
+}
+function backk(){
+	if(current!=0){
+		console.log(current)
+		load_student(current)
+		current--
+	}
+
 }
