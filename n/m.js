9b2b7b1116fba5d42a902eeeb8f369ce6e10de78
Alexander <olexander.pomazkin@yandex.ru>
2022-03-02 12:38:42 +0700

    13

--- n/m.js
+++ n/m.js
@@ -1,5 +1,5 @@
 let students = []
-let current = 0
+let max_id = -1
 
 function LoadFromSite() {
 	$.get('http://217.71.129.139:4035/students.php', function(data){
@@ -10,14 +10,17 @@ function LoadFromSite() {
 function show_table() {
 	let table = $('#tbl_all')
 	for (let i=0; i < students.length; i++) {
+		if (i > max_id) {
+			max_id = i
 		let tr = $('<tr></tr>')
-
+		
 		let td1 = $('<td>' + students[i].id + '</td>')
 		let td2 = $('<td>' + students[i].surname + '</td>')
 		let td3 = $('<td>' + students[i].name + '</td>')
-
+		
 		tr.append(td1).append(td2).append(td3)
-
+		
 		table.append(tr)
+		}
 	}
 }
