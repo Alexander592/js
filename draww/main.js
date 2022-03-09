46c4163a4f12cdec30335b5221227b5dc84b2147
Alexander <olexander.pomazkin@yandex.ru>
2022-03-09 11:24:02 +0700

    dr16

--- draww/main.js
+++ draww/main.js
@@ -5,11 +5,20 @@ function draw(){
 	let ctx = canvas.getContext('2d');
 	let pi = Math.PI;
 
-	ctx.beginPath();
+	 ctx.beginPath();
 	ctx.lineWidth = 7;
 	ctx.strokeStyle = "green";
 	ctx.fillStyle="yellow";
 	ctx.arc(150, 150, 85, 0, 2*pi, false);
+	ctx.stroke();
+	ctx.fill();
+
+
+	ctx.beginPath();
+	ctx.lineWidth = 7;
+	ctx.strokeStyle = "gray";
+	ctx.fillStyle="pink";
+	ctx.arc(250, 150, 85, 0, 2*pi, false );
 	ctx.stroke();
 	ctx.fill();    
 }
