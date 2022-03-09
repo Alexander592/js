25410a04abf6f724119148fab51d7bc3eea20289
Alexander <olexander.pomazkin@yandex.ru>
2022-03-09 11:30:38 +0700

    dr22

--- draww/main.js
+++ draww/main.js
@@ -3,17 +3,17 @@ function draw(){
 		 // Получение элемента canvas, контекста и свойства Math.PI
 	let canvas = document.getElementById('canva');
 	let ctx = canvas.getContext('2d');
-	let positionX = 75; // начальная позиция X координаты
+	let size = 75; // начальная позиция X координаты
 
 	setInterval(function() {
-	    ctx.beginPath();
-	    ctx.clearRect(0,0,600,300); // очистка холста
-	    ctx.fillStyle = "pink"; // цвет заливки
-	    ctx.rect(positionX, 75, 75, 75); // x, y, width, height
-	    ctx.fill();
-	    positionX++; // изменение позиции
-	    if(positionX > 600) {
-	        positionX = 75;
-	    }
-	},60); // запуск каждые 60 миллисекунд 
+    ctx.beginPath();
+    ctx.clearRect(0,0,600,300); // очистка холста
+    ctx.fillStyle = "pink"; // цвет заливки
+    ctx.rect(75, 75, size, size); // x, y, width, height
+    ctx.fill();
+    size++; // увеличение размера
+    if(size > 225) {
+        size = 75;
+    }
+	},60); // запуск каждые 60 миллисекунд  
 }
