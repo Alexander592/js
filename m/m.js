28927f479ef35de412f4c0ab86a3adc0794adda3
Alexander <olexander.pomazkin@yandex.ru>
2022-03-16 11:53:45 +0700

    глава 1 задание сделай сам

--- m/m.js
+++ m/m.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-   $("div").draggable();
+$(document).ready(function(){
+
+$(document).ready(function(){
+   $("#head").resizable();
 });
-$(document).ready(function() {
-   $("#drag1").draggable({axis:"x"});
-   $("#drag2").draggable({axis:"y"});
+
 });
